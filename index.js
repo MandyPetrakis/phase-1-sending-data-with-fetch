f5508e1b9e9b3057863827d9a5fda78196a0a88d
Mandy Petrakis <mandypetrakis@gmail.com>
2022-12-05 18:58:15 -0800

    commit tree eaabba0b05c64720df1e5e8f5cbf6bd6b4d724af

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((obj) => {
+      let li = document.createElement("li");
+      li.textContent = obj.id;
+      document.querySelector("body").appendChild(li);
+    })
+    .catch((message) => {
+      let alert = document.createElement("li");
+      alert.innerHTML = message;
+      document.querySelector("body").append(alert);
+    });
+}
